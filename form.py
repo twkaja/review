2d6a8f28bab02d986f6291f8b99b1fb14a09d690
unknown <1075519408@qq.com>
2018-03-16 11:46:52 +0800

    new

---
+++ form.py
@@ -0,0 +1,30 @@
+# -*- coding:utf-8 -*-
+
+from flask import Flask, render_template, redirect, url_for
+from flask_script import Manager
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+app = Flask(__name__)
+manager = Manager(app)
+
+app.config['SECRET_KEY'] = 'a hard string to guess'
+
+#表单类
+class NameForm(FlaskForm):
+    name = StringField('What\'s your name?', validators=[DataRequired(), ])
+    submit = SubmitField('Submit')
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+
+    form = NameForm()
+
+    return render_template('index.html', form=form)
+@app.route('/user/<name>', methods=['GET', 'POST'])
+def user(name):
+    return render_template('user.html', name=name)
+
+if __name__ == '__main__':
+    app.run()
